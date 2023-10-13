72c3df777e5bc24def2fbce05e512e1b4c9705e1
arttukauppinen <kauppinen27@gmail.com>
2023-10-13 19:09:23 +0300

    4.20

--- utils/middleware.js
+++ utils/middleware.js
@@ -26,8 +26,19 @@ const errorHandler = (error, request, response, next) => {
   next(error);
 };
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get("authorization");
+  console.log("authorization", authorization);
+  if (authorization && authorization.startsWith("Bearer ")) {
+    request.token = authorization.replace("Bearer ", "");
+    console.log("token", request.token);
+  }
+  next();
+};
+
 module.exports = {
   requestLogger,
   unknownEndpoint,
   errorHandler,
+  tokenExtractor,
 };
